46e0667b30689eba771746b33385c95b9c8e5731
Sakshi_Kumari <sakshitakkar5@gmail.com>
2022-08-05 11:03:41 +0530

    5 aug,2022 commit ist time

--- src/test/java/org/example/AppTestNew.java
+++ src/test/java/org/example/AppTestNew.java
@@ -6,12 +6,9 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.idealized.Javascript;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 
